79fed1f2095a817b6cf044524d53480cc47b8d5e
thakurpuru <purushottamthakur78@gmail.com>
2025-07-10 21:26:25 +0530

    Time: 327 ms (89.15%) | Memory: 12.4 MB (97.06%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -4,28 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        def check(st):
-            i=0
-            j=len(st)-1
-            while i<j:
-                if st[i]!=st[j]:
-                    return False
-                i+=1
-                j-=1
-            return True
-        if check(s):
-            return s
-        i=0
-        j=len(s)-1
-        res=""
-        while i<len(s):
-            for j in range(len(s),i,-1):
-                st=s[i:j]
-                if check(st):
-                    if len(st)>len(res):
-                        res=st
-            i+=1
-        return res
+        def check(st,i,j):
+            while i>=0 and j<len(st) and st[i]==st[j]:
+                j+=1
+                i-=1
+            return j-i-1
+        
+        start=0
+        end=0
+        for i in range(len(s)):
+            odd=check(s,i,i)
+            even=check(s,i,i+1)
+            max_len=max(odd,even)
+            if max_len>end-start:
+                start=i-(max_len-1)//2
+                end=i+max_len//2
+            
+        return s[start:end+1]
 
         
 
