10bb1fd371de3dbc80aac89716f4dbcce45351ad
thakurpuru <purushottamthakur78@gmail.com>
2025-08-05 18:18:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (64.10%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -4,21 +4,9 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        st=s.strip()
-        i=len(st)-1
-        mid=""
-        res=""
-        while i>=0:
-            
-            if st[i]==' ':
-                if mid!="" and mid!=" ":
-                    res+=(mid[::-1]+' ')
-                    mid=""
-            else:
-                mid+=st[i]
-            i-=1
-        res+=(mid.strip()[::-1])
-        return res.strip()
+        st=s.strip().split()
+        sr=st[::-1]
+        return " ".join(sr)
 
 
 
