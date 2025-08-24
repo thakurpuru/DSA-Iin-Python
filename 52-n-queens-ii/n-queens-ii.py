a378f8f3f2f89eee7e46299a7b233fb6bd3ca62d
thakurpuru <purushottamthakur78@gmail.com>
2025-08-24 17:22:57 +0530

    Time: 47 ms (20.61%) | Memory: 12.6 MB (18.47%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def totalNQueens(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        board=[['.']*n for _ in range(n)]
+        def issafe(i,j,board):
+            for row in range(i):
+                if board[row][j]=='Q':
+                    return False
+            
+            r,c=i-1,j-1
+            while r>=0 and c>=0:
+                if board[r][c]=='Q':
+                    return False
+                r-=1
+                c-=1
+            
+            k,l=i-1,j+1
+            while k>=0 and l<n:
+                if board[k][l]=='Q':
+                    return False
+                k-=1
+                l+=1
+            return True
+        self.res=0
+        def QueenPlace(row,board):
+            if row==n:
+                self.res+=1
+                return
+            for j in range(n):
+                if issafe(row,j,board):
+                    board[row][j]='Q'
+                    QueenPlace(row+1,board)
+                    board[row][j]='.'
+        QueenPlace(0,board)
+        return self.res
+        
