5d3f2fed6b5eeb398f614e7d932125c3723c6354
thakurpuru <purushottamthakur78@gmail.com>
2026-02-11 20:24:22 +0530

    Time: 3 ms (84.22%) | Memory: 12.4 MB (87.70%) - LeetSync

---
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def rangeBitwiseAnd(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        
+        i=0
+        while left!=right:
+            left>>=1
+            right>>=1
+            i+=1
+        return left<<i
+        
+             
+        
