55d5e618a4967d677c5c5d8d1460958b90c14336
thakurpuru <purushottamthakur78@gmail.com>
2026-07-10 15:32:28 +0530

    Time: 67 ms (8.66%) | Memory: 157.3 MB (16.47%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
+    vector<vector<int>>dp;
     int solve(int n,long long clip,long long screen){
         if(screen==n) return 0;
 
         if(screen>n || clip>n) return INT_MAX;
+        if(dp[clip][screen]!=-1) return dp[clip][screen];
         int ans=INT_MAX;
+
         if(clip>0){
             int a=solve(n,clip,screen+clip);
             if(a!=INT_MAX){
@@ -16,10 +19,11 @@ public:
             if(b!=INT_MAX) ans=min(b+1,ans);
 
         }
-        return ans;
+        return dp[clip][screen]=ans;
 
     }
     int minSteps(int n) {
+        dp.assign(n,vector<int>(n,-1));
         if(n==1) return 0;
         return solve(n,0,1);
     }
