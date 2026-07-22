71319149a7b768492ba44c1a53c466f478590767
thakurpuru <purushottamthakur78@gmail.com>
2026-07-22 11:50:03 +0530

    Time: 11 ms (49.41%) | Memory: 30.9 MB (64.37%) - LeetSync

---
+++ 221-maximal-square/maximal-square.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        int max_side=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]=='1'){
+                    dp[i+1][j+1]=min({dp[i][j],dp[i+1][j],dp[i][j+1]})+1;
+                    max_side=max(max_side,dp[i+1][j+1]);
+                }
+            }
+        }
+        return max_side*max_side;
+    }
+};
