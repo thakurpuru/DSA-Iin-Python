45f07c02a41d649c0f25edac10c413b3d5428399
thakurpuru <purushottamthakur78@gmail.com>
2026-08-10 21:03:23 +0530

    Time: 5 ms (42.68%) | Memory: 18.9 MB (34.42%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
-    bool cycle(int u,vector<bool>& vis,vector<bool>& st,vector<vector<int>>& g){
-        vis[u]=true;
-        st[u]=true;
-        for(int v:g[u]){
-            if(!vis[v]){
-                if(cycle(v,vis,st,g)) return true;
-
-            }else if(st[v]) return true;
-        }
-        st[u]=false;
-        return false;
-    }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         int n=numCourses;
         vector<vector<int>> g(n);
-        vector<bool> vis(n,false),st(n,false);
+        
         vector<int> indegree(n,0);
         for(auto u:prerequisites){
             g[u[1]].push_back(u[0]);
             indegree[u[0]]++;
         }
-        for(int i=0;i<n;i++){
-            if(!vis[i] && cycle(i,vis,st,g)) return {};
-        }
+       
         queue<int> q;
         for(int i=0;i<n;i++){
             if(indegree[i]==0){
@@ -42,6 +28,7 @@ public:
                 }
             }
         }
+        if(ans.size()!=n)return {};
         return ans;
     }
 };
